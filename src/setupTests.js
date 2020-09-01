2d42a7cdadc2e5e8979ac7ba20170d5ef05b3ce4
spectralapps <56676505+spectralapps@users.noreply.github.com>
2020-09-01 01:12:12 -0300

    Setup tests

---
+++ src/setupTests.js
@@ -0,0 +1,13 @@
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import 'jest-enzyme';
+
+configure({ adapter: new Adapter() });
+
+const localStorageMock = {
+  getItem: jest.fn(),
+  setItem: jest.fn(),
+  removeItem: jest.fn(),
+  clear: jest.fn(),
+};
+global.localStorage = localStorageMock;
