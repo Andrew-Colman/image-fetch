d8eb9f691485b821db350ffeb49536e4b4425dda
spectralapps <56676505+spectralapps@users.noreply.github.com>
2021-07-25 06:39:01 -0300

    Add swiper styles

--- src/components/GlobalStyles.js
+++ src/components/GlobalStyles.js
@@ -4,11 +4,37 @@ const GlobalStyle = createGlobalStyle`
   body {
     margin: 0;
     padding: 0;
-background: #343a40;
+    background: #343a40;
   }
   html{
     scroll-behavior: smooth;
   }
+  .swiper-container {
+
+}
+
+.swiper-slide {
+  /* Center slide text vertically */
+  display: -webkit-box;
+  display: -ms-flexbox;
+  display: -webkit-flex;
+  display: flex;
+  -webkit-box-pack: center;
+  -ms-flex-pack: center;
+  -webkit-justify-content: center;
+  justify-content: center;
+  -webkit-box-align: center;
+  -ms-flex-align: center;
+  -webkit-align-items: center;
+  align-items: center;
+}
+
+.swiper-slide img {
+  display: block;
+  width: auto;
+  height: 50%;
+  object-fit: cover;
+}
 `;
 
 export default GlobalStyle;
