54e87b6d9ee1499eff93ac2b211e50585781857a
spectralapps <56676505+spectralapps@users.noreply.github.com>
2021-07-25 05:45:52 -0300

    Restructure files

--- src/components/useParams.js
+++
@@ -1,7 +0,0 @@
-import { useLocation } from 'react-router-dom';
-
-export default function useParams(param) {
-  const queryParams = new URLSearchParams(useLocation().search);
-
-  return queryParams.get(param);
-}
--- src/components/useToasts.js
+++
@@ -1,8 +0,0 @@
-import { toast } from 'react-toastify';
-
-export default function useToasts() {
-  const notify = (msg) =>
-    toast(msg, { type: 'dark', position: 'bottom-center', autoClose: 5000, Transition: 'slide' });
-
-  return [notify];
-}
