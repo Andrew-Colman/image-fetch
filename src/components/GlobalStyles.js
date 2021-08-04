090da1e1c5bc0f9b09b70d718209bdae03addaf3
spectralapps <56676505+spectralapps@users.noreply.github.com>
2021-08-04 08:18:30 -0300

    Remove swiper styles

--- src/components/GlobalStyles.js
+++ src/components/GlobalStyles.js
@@ -9,32 +9,12 @@ const GlobalStyle = createGlobalStyle`
   html{
     scroll-behavior: smooth;
   }
-  .swiper-container {
 
+
+.custom-control-label{
+  cursor: pointer;
 }
 
-.swiper-slide {
-  /* Center slide text vertically */
-  display: -webkit-box;
-  display: -ms-flexbox;
-  display: -webkit-flex;
-  display: flex;
-  -webkit-box-pack: center;
-  -ms-flex-pack: center;
-  -webkit-justify-content: center;
-  justify-content: center;
-  -webkit-box-align: center;
-  -ms-flex-align: center;
-  -webkit-align-items: center;
-  align-items: center;
-}
-
-.swiper-slide img {
-  display: block;
-  width: auto;
-  height: 50%;
-  object-fit: cover;
-}
 `;
 
 export default GlobalStyle;
